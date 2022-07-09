93f7478a8ed12873317bc505b3074d6b25db1a70
silviocoluccia <97475550+silviocoluccia@users.noreply.github.com>
2022-07-09 18:18:33 +0200

    ConnectionAndroid
    Connection Android

--- SeaBookMobileApp/app/src/main/java/com/example/seabook_mobileapp/MainActivity.java
+++ SeaBookMobileApp/app/src/main/java/com/example/seabook_mobileapp/MainActivity.java
@@ -6,32 +6,42 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     EditText etDate;
     DatePickerDialog.OnDateSetListener setListener;
     private Button buttonMain;
+    String jsonFromServer;
+    ListView listView;
 
+    private final static String URL = "http://192.168.1.16:8080/api/v1/stabilimenti/";
+    ArrayList<String> tutorialList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonMain = (Button) findViewById(R.id.buttonMain);
-        buttonMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity2();
-            }
-        });
 
         etDate = findViewById(R.id.et_date);
 
@@ -56,9 +66,148 @@ public class MainActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+        new FetchDataTask().execute(URL);
     }
-    public void openActivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+
+    private class FetchDataTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "con";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Json Data is downloading", Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpHandler sh = new HttpHandler();
+
+            String url = "http://192.168.1.16:8080/api/v1/stabilimenti";
+
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            jsonFromServer = jsonStr;
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    String books = jsonObj.toString();
+                } catch (final JSONException e) {
+                    //  Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Toast.makeText(getApplicationContext(), "Json parsing error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+                return jsonFromServer;
+
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String dataFetched) {
+            //parse the JSON data and then display
+            parseJSON(dataFetched);
+        }
+
     }
+
+    //GIO
+    private String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    private void parseJSON(String data){
+
+        try{
+            JSONArray jsonMainNode = new JSONArray(data);
+
+            int jsonArrLength = jsonMainNode.length();
+
+            for(int i=0; i < jsonArrLength; i++) {
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                String name = jsonChildNode.getString("name");
+                tutorialList.add(name);
+                String address = jsonChildNode.getString("address");
+                tutorialList.add(address);
+
+            }
+
+            // Get ListView object from xml
+            listView = (ListView) findViewById(R.id.list);
+
+            // Define a new Adapter
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, tutorialList);
+
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
+
+        }catch(Exception e){
+            Log.i("App", "Error parsing data" +e.getMessage());
+
+        }
+    }
+
+    /*public class CallAPI extends AsyncTask<String, String, String> {
+
+        public CallAPI(){
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String data = params[1]; //data to post
+            OutputStream out = null;
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                out.close();
+
+                urlConnection.connect();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }*/
 }
